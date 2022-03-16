71fe29bfe19a22079b1d88c0daf228f848614977
carmen <carmenduran@domenicoscarlatti.es>
2022-03-16 12:49:43 +0100

    Añadimos el fichero calculator2.java al repositorio local

---
+++ Calculator2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carmen
+ */
+public class Calculator {
+    public double add(double number1, double number2){ 
+ return number1 + number2; 
+ } 
+ public double subtract(double number1, double number2){ 
+ return number1 - number2; 
+ } 
+ public double multiply(double number1, double number2) { 
+ return number1 * number2; 
+ } 
+ public double divide(double number1, double number2){ 
+ return number1 / number2; 
+ } 
+}
